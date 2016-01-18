ba6abbd5bb84e79ab882cbe998e38fc2d66a40b0
sanerem <sanerem@gmail.com>
2016-01-18 17:13:43 -0500

    Initial Commit

--- lua - Copy/autorun/sh_main.lua
+++
@@ -1,8 +0,0 @@
-UpOrDownVoting = {}
-
-if SERVER then
-  AddCSLuaFile("upordownvoting/cl_upordownvoting.lua")
-  include("mapvoting/sv_mapvoting.lua")
-  end
-
-if CLIENT then include("upordownvoting/cl_upordownvoting.lua") end
